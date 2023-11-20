aac87b304ed7f5e00ab0a4378399937bf0292ff9
aisdams <marioaistwice@gmail.com>
2023-11-20 16:29:48 +0700

    :tada: first commit :tada:

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit sequi exercitationem blanditiis molestias aliquid tenetur hic explicabo soluta quod. Culpa, repellendus labore? Adipisci dignissimos at optio cum, quae repellat error?</div>
+  )
+}
+
+export default Home
