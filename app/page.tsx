43b91b95a553e8b6006285dc2e1d5cc6fa3f0d0f
aisdams <marioaistwice@gmail.com>
2023-11-21 10:24:17 +0700

    add navbar & footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
   return (
-    <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit sequi exercitationem blanditiis molestias aliquid tenetur hic explicabo soluta quod. Culpa, repellendus labore? Adipisci dignissimos at optio cum, quae repellat error?</div>
-  )
-}
+    <section className="flex-start flex-col paddinga mb-16">
+      <h1> Home</h1>
+      <h1>Categories</h1>
+      <h1>Post</h1>
+      <h1>Loadmore</h1>E
+    </section>
+  );
+};
 
-export default Home
+export default Home;
---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, accusantium
+      sit? Pariatur fugit recusandae eaque suscipit veritatis maxime similique,
+      dolor ad. Laudantium veniam repellat ipsa necessitatibus ipsum eligendi
+      voluptate modi?
+    </div>
+  );
+};
+
+export default Footer;
